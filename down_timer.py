953f4c414ac918d4a54f9b855333a20512b00e75
annin401 <cando85@icloud.com>
2019-04-04 16:06:41 +0900

    timer.py -> main.py

--- down_timer.py
+++ down_timer.py
@@ -1,15 +1,16 @@
 #! /usr/local/bin/python3
+#  down_timer.py
 #  utf-8
+#  This file is a part of "timer" made by annin
 import time
+import sys
 
 def down_timer(max_time):
     for i in range(max_time, 0, -1):  
         print(f"remaining: {i}sec")
         time.sleep(1)
         print("\033[1A", end='')
+    #TODO formatを整える
     #TODO 音をつける
     print("A timer finished!")
-
-
-
 
---
+++ main.py
@@ -0,0 +1,21 @@
+#! /usr/local/bin/python3
+#  main.py
+#  utf-8
+import time
+import re
+import sys
+import down_timer
+
+max_time = 0
+
+#python3から呼び出すときと，コマンドのようにでも呼び出せるようにする
+#TODO 正規表現で書き換える
+
+
+from down_timer import down_timer 
+down_timer(max_time)
+
+
+
+
+
