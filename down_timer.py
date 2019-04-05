adf658417105b07e1bd8aab895a44fa1c2dd4540
annin401 <cando85@icloud.com>
2019-04-05 17:41:01 +0900

    remove argument.py removed and formated down_timer
    remove reason: I did't know how to use this module(click)

--- down_timer.py
+++ down_timer.py
@@ -4,13 +4,15 @@
 import time
 
 def down_timer(max_time):
-
-
-    for i in range(max_time, 0, -1):  
-        print(f"remaining: {i}sec")
+    for i in range(max_time, -1, -1):  
+        hour = i // 3600
+        minute = i %  3600 // 60
+        second = i % 60 
+        print("remaining: {hour:02}h{minute:02}m{second:02}s"
+                .format(hour=hour, minute=minute, second=second))
         time.sleep(1)
         print("\033[1A", end='')
     #TODO formatを整える
     #TODO 音をつける
-    print("A timer finished!")
+    print("\nA timer finished!")
 
--- main.py
+++ main.py
@@ -2,11 +2,12 @@
 #   main.py
 #   codeing:UTF-8
 import down_timer
-import argument_process
+#import argument_process
 
-# 引数から整数のタプルを生成する
-time = argument_process.argument_process()
-
+#入力
+hour = input('時:')
+minute = input('分:')
+second = input('秒:')
+time = int(hour) * 3600 + int(minute) * 60 + int(second)
 # タイマーを実行する
-down_timer.down_timer(max_time)
-
+down_timer.down_timer(time)
